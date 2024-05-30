4f163e058c7dab67d1d4b50761f97e23cd7c13e7
DELL <sulaimabaliqis@gmail.com>
2024-05-30 16:26:42 +0100

    mavericktube

---
+++ maverickHub/src/main/java/com/mavericktube/maverickHub/MaverickHubApplication.java
@@ -0,0 +1,13 @@
+package com.mavericktube.maverickHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaverickHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaverickHubApplication.class, args);
+	}
+
+}
---
+++ maverickHub/src/test/java/com/mavericktube/maverickHub/MaverickHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavericktube.maverickHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaverickHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
